ef588201abe2fc8dd470711d32dc64da51e5c4d2
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-10 08:39:25 +0530

    [Rahmath]Add: Program For Parenthesis

---
+++ GenericsPrograms/Parenthesis.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsWithDataStructures.GenericsPrograms
+{
+    public class Parenthesis<T>
+    {
+        public void ParenthesesChecking<T>()
+        {
+            int top = -1;
+            int[] str = new int[17] { '(', '5', '+', '6', ')', '*', '(', '7', '+', '8', ')', '/', '(', '4', '+', '3', ')' };
+            int n = str.Length;
+            char[] stack = new char[17];
+            for (int i = 0; i < n; i++)
+            {
+                if (str[i] == '(')
+                {
+                    top = top + 1;
+                    stack[top] = '(';
+                }
+                if (str[i] == ')' && stack[top] == '(')
+                {
+                    top = top - 1;
+                }
+            }
+            if (top == -1)
+            {
+                Console.WriteLine("Balanced Parenthesis");
+            }
+            else
+            {
+                Console.WriteLine("Unbalanced Parenthesis");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using GenericsWithDataStructures.GenericsPrograms;
+
+namespace GenericsWithDataStructures
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Welcome to Generics Program----------");
+            bool end = true;
+            Console.WriteLine("\n1.Parentheses\n2.End Of Program ");
+            while (end)
+            {
+                Console.WriteLine("\nEnter Option For Execute The Program");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Parenthesis<int> parentheses = new Parenthesis<int>();
+                        parentheses.ParenthesesChecking<int>();
+                        break;
+                    case 2:
+                        end = false;
+                        Console.WriteLine("Program Is Ended.");
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Correct Option");
+                        break;
+                }
+            }
+        }
+    }
+}
